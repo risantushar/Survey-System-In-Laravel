6afd41a4fe91ccde3441eb2e28502ecbd4d75abb
Md Tohidul Alan <62894946+risantushar@users.noreply.github.com>
2021-06-05 20:00:36 +0600

    Survey Management System laravel

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Question;
+use App\Questionnaire;
+
+class QuestionController extends Controller
+{
+    public function create(Questionnaire $questionnaire)
+    {
+        return view('question.create', compact('questionnaire'));
+    }
+
+    public function store(Questionnaire $questionnaire)
+    {
+        // return $questionnaire;
+        $data = request()->validate([
+            'question.question' => 'required',
+            'answers.*.answer' => 'required',
+        ]);
+
+        $question = $questionnaire->questions()->create($data['question']);
+        // return $question;
+        $question->answers()->createMany($data['answers']);
+
+        return redirect('/questionnaires/'.$questionnaire->id);
+    }
+
+    public function destroy(Questionnaire $questionnaire, Question $question)
+    {
+        $question->answers()->delete();
+        $question->delete();
+
+        return redirect($questionnaire->path());
+    }
+}
